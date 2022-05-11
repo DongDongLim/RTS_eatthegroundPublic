2bfad62b09751a1937fdc954a55316f756298083
DongDongLim <99638155+DongDongLim@users.noreply.github.com>
2022-05-12 04:57:38 +0900

    다시커밋

---
+++ Assets/Dong/Script/Mng/UIMng.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+public class StringInt : SerializableDictionary<string, int> { }
+
+[System.Serializable]
+public class StringObj : SerializableDictionary<string, GameObject> { }
+public class UIMng : Singleton<UIMng>
+{
+    [SerializeField]
+    public StringObj uiList;
+
+    [SerializeField]
+    GameObject[] infoUI;
+
+    protected override void OnAwake()
+    {
+        SceneMng.instance.SceneEnter += DeActiveUI;
+        GameMng.instance.GameStart += OnStartBtnDisable;
+    }
+
+    public void DeActiveUI(string scene)
+    {
+        switch (scene)
+        {
+            case "FullMap":
+                break;
+            case "Town":
+                break;
+        }
+    }
+
+    public void OnStartBtnDisable()
+    {
+        if (uiList["이름설정"].GetComponent<Text>().text == "")
+        {
+            GameMng.instance.SetPlayerName("NoName");
+            uiList["플레이어이름"].GetComponentInChildren<Text>().text = "NoName";
+        }
+        else
+        {
+            GameMng.instance.SetPlayerName(uiList["이름설정"].GetComponent<Text>().text);
+            uiList["플레이어이름"].GetComponentInChildren<Text>().text = uiList["이름설정"].GetComponent<Text>().text;
+        }
+        uiList["플레이어이름"].transform.position = GameMng.instance.GetPlayerTransform().position + new Vector3(1, 3, -1);
+        uiList["플레이어이름"].SetActive(true);
+        uiList["게임시작"].SetActive(false);
+        uiList["미니맵"].SetActive(true);
+    }
+
+    public GameObject GetInfoUI(int index)
+    {
+        for (int i = 0; i < uiList["마을정보"].transform.childCount; ++i)
+            uiList["마을정보"].transform.GetChild(i).gameObject.SetActive(false);
+        uiList["마을정보"].SetActive(true);
+        return infoUI[index];
+    }
+
+    public void OnBtnDisable()
+    {
+        EventSystem.current.currentSelectedGameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Dong/Script/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    float m_rotateSpd;
+
+    void Update()
+    {
+        transform.Rotate(0, m_rotateSpd * Time.deltaTime, 0);
+    }
+}
